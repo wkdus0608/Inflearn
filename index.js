802fd5c218f5f42a5f4495ba59be6c9a42f624f1
wkdus0608 <wkdus0608@naver.com>
2025-01-31 15:36:10 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://Jeon:abcd1234@cluster0.zh30l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
