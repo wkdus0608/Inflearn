ecd1fbb535a9604aff9e93179ff68e1cab88372f
wkdus0608 <seongmin020608@gmail.com>
2025-02-05 20:45:56 +0900

    React Router Dom

--- JohnAnn/nodeAndExpress/client/src/App.js
+++ JohnAnn/nodeAndExpress/client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+        </ul> */}
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/register' component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
